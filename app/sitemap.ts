b388cc3f5e0a4e42f132d5821109258706dfdaf1
jay6o <richforeverrr6@proton.me>
2025-06-12 17:58:24 -0400

    sitemap url

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next"
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https:/localhost:3000",
+      url: "https://agency-itde.vercel.app",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1.0,
